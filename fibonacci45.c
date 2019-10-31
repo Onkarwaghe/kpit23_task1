76316ffa5182ff7d907d6463f4d3e020c44ce4ff
Onkarwaghe <57176901+Onkarwaghe@users.noreply.github.com>
2019-10-31 08:37:04 +0530

    Create fibonacci45.c

---
+++ fibonacci45.c
@@ -0,0 +1,16 @@
+//...error-no library file added...//
+int main()
+{
+    int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series: ");
+    for (i = 1 i <= n ++i) //...no termination in for loop...//
+    {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    return 0;
+}
